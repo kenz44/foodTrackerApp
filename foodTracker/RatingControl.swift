c1092f257f3bf6bac7246408f25dc52673a7e616
Kenz Bauer <mabauer@ualberta.ca>
2021-03-25 21:27:02 -0600

    Added a working button.

---
+++ foodTracker/RatingControl.swift
@@ -0,0 +1,48 @@
+//
+//  RatingControl.swift
+//  foodTracker
+//
+//  Created by Kenz Bauer on 2021-03-25.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed.")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add Contraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+        
+    }
+
+}
