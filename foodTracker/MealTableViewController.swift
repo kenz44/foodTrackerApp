c788a10f2418cd4d3de2b23e3375b3e7123dd4c6
Kenz Bauer <mabauer@ualberta.ca>
2021-03-28 19:22:44 -0600

    Added title and plus button.

--- foodTracker/MealTableViewController.swift
+++ foodTracker/MealTableViewController.swift
@@ -103,15 +103,15 @@ class MealTableViewController: UITableViewController {
         let photo2 = UIImage(named: "meal2")
         let photo3 = UIImage(named: "meal3")
         
-        guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
+        guard let meal1 = Meal(name: "Gnocchi", photo: photo1, rating: 4) else {
             fatalError("Unable to instantiate meal1")
         }
          
-        guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
+        guard let meal2 = Meal(name: "Burger and Fries", photo: photo2, rating: 5) else {
             fatalError("Unable to instantiate meal2")
         }
          
-        guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3) else {
+        guard let meal3 = Meal(name: "French Toast", photo: photo3, rating: 3) else {
             fatalError("Unable to instantiate meal2")
         }
         
